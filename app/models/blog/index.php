208bfbb27b51d7d3772a9043b6612a8e23f428a3
panda19931998 <seri19931998@yahoo.co.jp>
2019-02-20 20:32:54 +0900

    冒頭部分と末尾部分の共通化

--- app/models/blog/index.php
+++ app/models/blog/index.php
@@ -1,33 +1,4 @@
-<!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
-<!--[if !IE]><!-->
-<html lang="en">
-<!--<![endif]-->
-<head>
-<?php include(TEMPLATE_PATH."/head_tag.php"); ?>
-</head>
-<body>
-	<!-- begin #page-loader -->
-	<?php include(TEMPLATE_PATH."/page_loader.php"); ?>
-	<!-- end #page-loader -->
-
-	<!-- begin #page-container -->
-	<div id="page-container" class="page-container fade page-sidebar-fixed page-header-fixed">
-		<!-- begin #header -->
-		<?php include(TEMPLATE_PATH."/header.php"); ?>
-		<!-- end #header -->
-
-		<?php include(TEMPLATE_PATH."/sidebar.php"); ?>
-
-
-		<!-- begin #content -->
-		<div id="content" class="content">
-			<!-- begin breadcrumb -->
-			<?php include(TEMPLATE_PATH."/breadcrumb.php"); ?>
-			<!-- end breadcrumb -->
-			<!-- begin page-header -->
-			<h1 class="page-header">Blank Page <small>header small text goes here...</small></h1>
-			<!-- end page-header -->
+            <?php include(TEMPLATE_PATH."/template_head.php"); ?>
 			<!-- begin panel -->
 			<div class="panel panel-inverse">
 				<div class="panel-heading">
@@ -44,20 +15,4 @@
 				</div>
 			</div>
 			<!-- end panel -->
-		</div>
-		<!-- end #content -->
-
-		<!-- begin theme-panel -->
-
-		<!-- end theme-panel -->
-
-		<!-- begin scroll to top btn -->
-		<?php include(TEMPLATE_PATH."/scroll_top.php"); ?>
-		<!-- end scroll to top btn -->
-	</div>
-	<!-- end page container -->
-
-	<!-- ================== BEGIN BASE JS ================== -->
-	<?php include(TEMPLATE_PATH."/scripts.php"); ?>
-</body>
-</html>
+			<?php include(TEMPLATE_PATH."/template_bottom.php"); ?>
--- config.php
+++ config.php
@@ -7,3 +7,5 @@ mb_internal_encoding("UTF-8");
 define('CONTENTS_SERVER_URL', 'http://c.blog-system-5884.localhost');
 
 define('TEMPLATE_PATH', 'C:/Dropbox/develop/blog-system-5884/app/templates');
+
+define('COPYRIGHT', '&copy;'.date("Y").' SENSE SHARE All Rights Reserved.');
