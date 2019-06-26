959b9997b06440b89ed3d8161c7281dfdede2c42
panda19931998 <seri19931998@yahoo.co.jp>
2019-06-26 17:30:37 +0900

    カテゴリー削除機能修正４

--- app/models/blog/delete.php
+++ app/models/blog/delete.php
@@ -2,7 +2,7 @@
 
 $id = $_GET['id'];
 //blog_category_master_idの取得
-$sql = "select * from blog_category_master where blog_category_code = :blog_category_code and client_id = :client_id blog_id =:blog_id limit 1";
+$sql = "select * from blog_category_master where blog_category_code = :blog_category_code and client_id = :client_id and blog_id =:blog_id limit 1";
 $stmt = $pdo->prepare($sql);
 $params = array(
 	":blog_category_code" => $id,
@@ -13,12 +13,9 @@ $stmt->execute($params);
 $blog_category_master = $stmt->fetch();
 
 //blog_category_masterのカテゴリーを削除
-$sql = "delete from blog_category_master where client_id =:client_id and blog_id =:blog_id and blog_category_code = :blog_category_code";
+$sql = "delete from blog_category_master where id =:id";
 $stmt = $pdo->prepare($sql);
-$stmt->execute(array(":blog_category_code" => $id,
-					":client_id" => $user['id'],
-					":blog_id" =>  $blog['id'])
-);
+$stmt->execute(array(":id" => $blog_category_master['id']));
 
 //blog_categoryのカテゴリーを削除
 $sql = "delete from blog_category where blog_category_master_id = :blog_category_master_id";
