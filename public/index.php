49f6ef442340393e088432a9c3094ebf88d186dd
panda19931998 <seri19931998@yahoo.co.jp>
2022-04-08 19:06:02 +0900

    コントローラー部分（index.php)修正

--- public/index.php
+++ public/index.php
@@ -16,10 +16,11 @@ try {
 
 		// 物理ディレクトリURL取得
 
-		$uri = rtrim($_SERVER["REQUEST_URI"], '/');
-		$client_code = substr($uri, strrpos($uri, '/') + 1);
+//		$uri = rtrim($_SERVER["REQUEST_URI"], '/');
+//		$client_code = substr($uri, strrpos($uri, '/') + 1);
 
-
+		$path_arr = explode('/',$_SERVER['REQUEST_URI']);
+		$client_code = $path_arr[1];
 
 		$sql = "select * from client where client_code = :client_code limit 1";
 		$stmt = $pdo->prepare($sql);
@@ -42,18 +43,32 @@ try {
 
 		echo h($blog_id);
 
-    	if(substr($request_path , -4) =='html' or substr($request_path , -5) =='html/'){
 
+		if($request_path == '/'.$client_code.'/entry'){
+			$include_program ='/entry.php';
+			include(dirname(__FILE__).'/models/blog'.$include_program);
+
+		// ファイル名が「style.php」だった場合
+		} elseif ($request_path == '/'.$client_code.'/style.css') {
+			$include_program = '/style.php';
+			include(dirname(__FILE__).'/models/blog'.$include_program);
+		} elseif ($request_path == '/'.$client_code.'/category') {
+			$include_program = '/list.php';
+			include(dirname(__FILE__).'/models/blog'.$include_program);
+		} elseif ($request_path == '/'.$client_code.'/image') {
+			$include_program = '/image.php';
+			include(dirname(__FILE__).$client_code.$include_program);
+		} elseif ($request_path == '/'.$client_code.'/feed') {
+			$include_program = '/feed.php';
+			include(dirname(__FILE__).'/models/blog'.$include_program);
+		} elseif ($request_path == '/'.$client_code.'/') {
+			$include_program = '/list.php';
+			include(dirname(__FILE__).'/models/blog'.$include_program);
+		} elseif(substr($request_path , -4) =='html' or substr($request_path , -5) =='html/'){
 			include(dirname(__FILE__).'/models/blog/entry.php');
-    	} else {
-
-			if (isset($url_list[$request_path])) {
-				// アクセスされたURLのプログラムに処理を移譲
-				include(dirname(__FILE__).$url_list[$request_path]);
-        	} else {
-				include(dirname(__FILE__).'/models/blog/error.php');
-			}
-    	}
+		} else {
+			include(dirname(__FILE__).'/models/blog/error.php');
+		}
 	}
 	unset($pdo);
 } catch (Exception $e) {
